0d2efdfa40d66dc46f852a128fbe3969bd542349
richardGra <xgrane00@stud.fit.vutbr.cz>
2018-05-21 08:24:22 +0200

    Change return type of generateID func from []byte to string

--- utils.go
+++ utils.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"crypto/rand"
+	"fmt"
 	"io"
 )
 
-func generateID() []byte {
+func generateID() string {
 	id := make([]byte, 16)
 	r := rand.Reader
 	if _, err := io.ReadFull(r, id); err != nil {
 		panic(err)
 	}
-	return id
+	stringID := (fmt.Sprintf(`%02X`, id))
+	return stringID
 }
